725f26aa83d83740a00ad69c09d05e3bf0df2aee
Karla Medina <karlitafuentes22@gmail.com>
2022-06-08 15:11:40 -0500

    space
    nada en especifico sólo estética

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
   // Form calculadora- Historial
   $('#form-calculadora').submit(function (){
-    if($('#operacion').val() =='Division'){
+    if($('#operacion').val() == 'Division'){
       if (($("#numero1").val() <= 0) || ($("#numero2").val() <= 0)){
         $("#modal-division").modal('show');
       }
